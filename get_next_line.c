e97e068eaa347ef1c6f6913ea2b6cdcdb77bd2a4
Issam El Khaili <isel-kha@e2r1p12.1337.ma>
2025-01-07 12:35:50 +0100

    good update

--- get_next_line.c
+++ get_next_line.c
@@ -11,44 +11,14 @@
 /* ************************************************************************** */
 #include "get_next_line.h"
 
-char	*ft_extract_line(char *str)
-{
-	char	*line;
-	int		i;
-
-	if (!str)
-		return (NULL);
-	i = 0;
-	while (str[i] != '\n' && str[i] != '\0')
-		i++;
-	line = malloc(i + 2);
-	if (!line)
-		return (NULL);
-	i = 0;
-	while (str[i] != '\n' && str[i] != '\0')
-	{
-		line[i] = str[i];
-		i++;
-	}
-	if (str[i] == '\n')
-	{
-		line[i] = '\n';
-		i++;
-	}
-	line[i] = '\0';
-	return (line);
-}
-
-char	*ft_read_file(int fd)
+static char	*ft_read_file(int fd, char *stash)
 {
 	char	*buffer;
-	char	*stash;
 	int		bytes_read;
 
 	buffer = malloc(BUFFER_SIZE + 1);
 	if (!buffer)
 		return (NULL);
-	stash = NULL;
 	bytes_read = 1;
 	while (!ft_strchr(stash, '\n') && bytes_read > 0)
 	{
@@ -67,17 +37,78 @@ char	*ft_read_file(int fd)
 	return (stash);
 }
 
-char	*get_next_line(int fd)
+static char	*ft_extract_line(char *stash)
 {
-	char	*stash;
 	char	*line;
+	int		i;
 
-	if (fd < 0 || BUFFER_SIZE <= 0 || BUFFER_SIZE > INT_MAX)
-		return (NULL);
-	stash = ft_read_file(fd);
 	if (!stash)
 		return (NULL);
-	line = ft_extract_line(stash);
+	i = 0;
+	while (stash[i] != '\n' && stash[i] != '\0')
+		i++;
+	line = malloc(i + 2);
+	if (!line)
+		return (NULL);
+	i = 0;
+	while (stash[i] != '\n' && stash[i] != '\0')
+	{
+		line[i] = stash[i];
+		i++;
+	}
+	if (stash[i] == '\n')
+	{
+		line[i] = '\n';
+		i++;
+	}
+	line[i] = '\0';
+	return (line);
+}
+
+static char	*ft_update_stash(char *stash)
+{
+	int		i;
+	char	*new_stash;
+
+	i = 0;
+	if (!stash)
+		return (NULL);
+	while (stash[i] != '\n' && stash[i] != '\0')
+		i++;
+	if (stash[i] == '\n')
+		i++;
+	if (stash[i] == '\0')
+	{
+		free(stash);
+		return (NULL);
+	}
+	new_stash = ft_strjoin(NULL, &stash[i]);
 	free(stash);
+	return (new_stash);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*leftover;
+	char		*line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0 || BUFFER_SIZE > INT_MAX)
+	{
+		free(leftover);
+		leftover = NULL;
+		return (NULL);
+	}
+	if (!leftover)
+		leftover = ft_read_file(fd, leftover);
+	if (!leftover)
+		return (NULL);
+	line = ft_extract_line(leftover);
+	if (!line)
+	{
+		free(leftover);
+		leftover = NULL;
+		return (NULL);
+	}
+	leftover = ft_update_stash(leftover);
 	return (line);
 }
